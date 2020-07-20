df16c8ba256b6ef65357b24c8d002c79a9a83101
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2020-07-20 19:43:38 +0530

    added build folder

---
+++ build/precache-manifest.68a02df4b19b80953f7ecfd9f01fd660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cbfce96bc0c65c916a134337a7d091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98f78e94480402292bf",
+    "url": "/static/css/main.ae004e70.chunk.css"
+  },
+  {
+    "revision": "bb2b3139859b70b622c6",
+    "url": "/static/js/2.dd445ee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd445ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98f78e94480402292bf",
+    "url": "/static/js/main.53bdaf80.chunk.js"
+  },
+  {
+    "revision": "c13cf9836c44ed3c1b3a",
+    "url": "/static/js/runtime-main.5bffadda.js"
+  }
+]);
